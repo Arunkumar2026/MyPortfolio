a443f26ee015cc009c3e073af3d684367e440947
Arun Kumar <akulakumar453@gmail.com>
2026-05-22 10:45:56 +0530

    this is App file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import Projects from './pages/Projects'
 import { Toaster } from 'react-hot-toast'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className='flex flex-col min-h-screen'>
       <Navbar/>
       <main className='flex-1'>
-        <Home/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/projects' element={<Projects/>}/>
+        </Routes>
       </main>
       <Toaster/>
     </div>
