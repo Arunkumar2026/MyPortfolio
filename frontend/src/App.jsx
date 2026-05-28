f2f674bf1f3940bc2764d1315e1d95fd6a371e1c
Arun Kumar <akulakumar453@gmail.com>
2026-05-28 19:31:09 +0530

    this is App file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home'
 import Projects from './pages/Projects'
 import { Toaster } from 'react-hot-toast'
 import { Routes, Route } from 'react-router-dom'
+import Contact from './pages/Contact'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/projects' element={<Projects/>}/>
+          <Route path='/contact' element={<Contact/>}/>
         </Routes>
       </main>
       <Toaster/>
